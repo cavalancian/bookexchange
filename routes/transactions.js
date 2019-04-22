6cc4f01494a4ef11287537b23c3a8be22285eb89
Nick <ncavalancia@gmail.com>
2019-04-21 22:49:19 -0400

    Added mail seller functionality

--- routes/transactions.js
+++ routes/transactions.js
@@ -4,13 +4,13 @@ var Book = require("../models/book");
 var User = require("../models/user");
 var Transaction = require("../models/transaction");
 var middleWare = require("../middleware");
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
  service: 'gmail',
  auth: {
-        user: 'calubookex@gmail.com',
-        pass: 'bookitwithcal'
+        user: process.env.EMAIL_LOGIN,
+        pass: process.env.EMAIL_PASS
     }
 });
 //buy book route
@@ -24,7 +24,6 @@ router.post("/:id/buy", middleWare.isLoggedIn, function (req, res) {
            //mark book as sold
            foundBook.sold = true;
            foundBook.save();
-           console.log(foundBook);
            //create the new transaction
            var transaction = new Transaction ({
                buyer: {
@@ -51,33 +50,33 @@ router.post("/:id/buy", middleWare.isLoggedIn, function (req, res) {
                buyer.save();
            });
            //and seller transactions
-           User.findById(foundBook.author.id, function (err, seller) {
+           User.findById(foundBook.author.id, async function (err, seller) {
               if(err) {
                   req.flash("error", "Error finding user during transaction");
                   return res.redirect("/books");
               }
-              seller.transactions.push(transaction); //pushes seller
-              seller.save(); //saves it
+               seller.transactions.push(transaction); //pushes seller
+               await seller.save(); //saves it to database
+               //email the seller
+               const mailOptions = {
+                   from: 'calubookex@gmail.com',
+                   to: seller.username,
+                   subject: 'DONOTREPLY Your book ' + foundBook.name + ' has sold',
+                   html: '<h1>You\'ve sold a book!</h1><p>Please check your account transactions!</p>'
+               };
+               //send the mail!
+               await transporter.sendMail(mailOptions, function(error, info){
+                   if (error) {
+                       console.log(error);
+                       res.redirect("/books");
+                   } else {
+                       req.flash("success", "Thank you for your purchase. The seller should be contacting you soon!");
+                       res.redirect("/books");
+                   }
+               });
            });
        }
     });
-    //email seller of transaction
-    const mailOptions = {
-  from: 'calubookex@gmail.com', // just a email address to use
-  to: 'seller.username', // reciever name may need changed idk if this will work and can't test it without you
-  subject: 'DONOTREPLY Your book has sold', // subject to seller
-  html: '<p>Your html here</p>'// idk if we really need anything here if not marked for deletion
-    };   
-    //confirmation messages
-    transporter.sendMail(mailOptions, function (err) {
-   if(err)
-     req.flash("Error", "Problem sending email!");
-     res.redirect("/books");
-   else
-     req.flash("success", "Thank you for your purchase. Check your email!");
-    res.redirect("/books");
-    });
-    
 });
 
 module.exports = router;
